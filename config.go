173475043e0aff3f5504f9ba7fedccc50c7445c8
Justin Fenn <buggito@gmail.com>
2014-05-27 23:15:18 -0700

    look for hostfiles in user config directory

--- config.go
+++ config.go
@@ -3,8 +3,16 @@ package main
 import (
 	"flag"
 	"io/ioutil"
+	"os"
+	"path"
 
 	"gopkg.in/v1/yaml"
+)
+
+const (
+	XDG_CONFIG_HOME     = "$XDG_CONFIG_HOME"
+	DEFAULT_CONFIG_HOME = "$HOME/.config"
+	CONFIG_SUBDIR       = "dcmd"
 )
 
 type HostConfig struct {
@@ -29,7 +37,36 @@ func loadConfig() HostConfig {
 }
 
 func readHostfile() []byte {
-	filename := flag.Arg(0)
+	configFileParam := flag.Arg(0)
+	for _, file := range getConfigLocationCandidates(configFileParam) {
+		if exists(file) {
+			return getContents(file)
+		}
+	}
+	errLogger.Fatalf("hostfile %s not found", configFileParam)
+	return nil
+}
+
+func getConfigLocationCandidates(configFileParam string) []string {
+	var configRoot, configDir string
+	xdgHome := os.Getenv(XDG_CONFIG_HOME)
+	if xdgHome != "" {
+		configRoot = xdgHome
+	} else {
+		configRoot = os.ExpandEnv(DEFAULT_CONFIG_HOME)
+	}
+	configDir = path.Join(configRoot, CONFIG_SUBDIR)
+	return []string{configFileParam,
+		path.Join(configDir, configFileParam+".yml"),
+		path.Join(configDir, configFileParam+".yaml")}
+}
+
+func exists(filename string) bool {
+	_, err := os.Stat(filename)
+	return !os.IsNotExist(err)
+}
+
+func getContents(filename string) []byte {
 	fileContents, err := ioutil.ReadFile(filename)
 	if err != nil {
 		errLogger.Fatalln(err)
